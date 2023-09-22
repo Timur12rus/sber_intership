140df544e5131cebfb07931833fb0b84e9444033
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-09-22 12:26:13 +0300

    Add sort cities by name in reverse order

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 import models.City;
 import utils.CityParser;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         List<City> cities = CityParser.parse();
+        Collections.sort(cities, new Comparator<City>() {
+            @Override
+            public int compare(City o1, City o2) {
+                return o2.getName().compareTo(o1.getName());
+            }
+        });
         System.out.println(cities);
     }
 }
--- src/models/City.java
+++ src/models/City.java
@@ -66,4 +66,5 @@ public class City {
                 ", foundation='" + foundation + '\'' +
                 '}' + "\n";
     }
+
 }
