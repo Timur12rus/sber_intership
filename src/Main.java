8325eb208e56ab01172902182a7c33a8d21ea04a
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-09-25 12:02:26 +0300

    Add sort cities by name and sort cities by district and name

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,44 @@ public class Main {
     public static void main(String[] args) {
 
         List<City> cities = CityParser.parse();
+
+//      сортируем по названию города по убыванию в алфавитном порядке (реализация Comparator)
         Collections.sort(cities, new Comparator<City>() {
             @Override
             public int compare(City o1, City o2) {
-                return o2.getName().compareTo(o1.getName());
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        System.out.println(cities);
+
+//      сортируем по названию города по убыванию в алфавитном порядке (реализация Lambda)
+        cities.sort((o1, o2) -> {
+            return o1.getName().compareTo(o2.getName());
+        });
+        System.out.println(cities);
+
+//      сортируем по двум полям справочника – федеральному округу и наименованию города:
+        Collections.sort(cities, new Comparator<City>() {
+            @Override
+            public int compare(City o1, City o2) {
+                int cmp1 = o1.getDistrict().compareTo(o2.getDistrict());
+                if (cmp1 == 0) {
+                    return o1.getName().compareTo(o2.getName());
+                } else {
+                    return cmp1;
+                }
+            }
+        });
+        System.out.println(cities);
+
+//      сортируем по двум полям справочника – федеральному округу и наименованию города(реализация Lambda):
+        cities.sort((o1, o2) ->
+        {
+            int cmp1 = o1.getDistrict().compareTo(o2.getDistrict());
+            if (cmp1 == 0) {
+                return o1.getName().compareTo(o2.getName());
+            } else {
+                return cmp1;
             }
         });
         System.out.println(cities);
