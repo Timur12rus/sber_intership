6854b9f22c0f99bef42bdb38c41e3fea4d7e45cf
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-09-25 14:28:36 +0300

    поиск количества городов в разрезе регионов

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,21 @@
 import models.City;
 import utils.CityParser;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.text.MessageFormat;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         List<City> cities = CityParser.parse();
 
-        City[] citiesArray = cities.toArray(new City[0]);
-        int maxPopulation = 0;
-        int indexOfMaxPopulation = 0;
-        for (int i = 0; i < citiesArray.length; i++) {
-            if (citiesArray[i].getPopulation() > maxPopulation) {
-                maxPopulation = citiesArray[i].getPopulation();
-                indexOfMaxPopulation = i;
-            }
-        }
+        // поиск количества городов в разрезе регионов
+        quantityOfCitiesByRegionV1(cities);
+    }
 
-        System.out.println("[" + indexOfMaxPopulation + "] = " + maxPopulation);
+    private static void quantityOfCitiesByRegionV1(List<City> cities) {
+        Map<String, Integer> regions = new HashMap<>();
+        cities.forEach(city -> regions.merge(city.getRegion(), 1, Integer::sum));
+        regions.forEach((k, v) -> System.out.println(MessageFormat.format(" {0} = {1}", k, v)));
     }
 }
